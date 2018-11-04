c0c7ac04635ab9a251e70192306fefc569697edf
shweyeetub <syt@mit.com.mm>
2018-11-04 20:18:49 +0630

    Initial Commit

---
+++ BookStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BookStore
+//
+//  Created by Shwe Yi Tun on 11/4/18.
+//  Copyright © 2018 Shwe Yi Tun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
